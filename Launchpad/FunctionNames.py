39815cb510ae4132a1ed105cd9551f4124a06692
Stefan Schmelz <stefan_schmelz@gmx.net>
2021-07-01 21:41:20 +0200

    Started implementing Launchpad Fields and constants.

---
+++ Launchpad/FunctionNames.py
@@ -0,0 +1,15 @@
+FUNCTION_NAMES = {
+    'LED_PALETTE': 10,
+    'LED_RGB': 11,
+    'COLUMN_PALETTE': 12,
+    'ROW_PALETTE': 13,
+    'ALL_PALETTE': 14,
+    'SCROLL_TEXT': 20,
+    'SCROLL_FINISHED': 21,
+    'CHANGE_LAYOUT': 34,
+    'FLASH_PALETTE': 35,
+    'PULSE_PALETTE': 40,
+    'FADER_SETUP': 43
+}
+
+FUNCTION_IDS = {key: value for value, key in FUNCTION_NAMES.items()}
---
+++ Launchpad/LayoutNames.py
@@ -0,0 +1,11 @@
+LAYOUT_IDS = {
+    'SESSION': 0,
+    'USER1': 1,
+    'USER2': 2,
+    'LIVE': 3,
+    'VOLUME': 4,
+    'PAN': 5
+}
+
+
+LAYOUT_NAMES = {key: value for value, key in LAYOUT_IDS.items()}
